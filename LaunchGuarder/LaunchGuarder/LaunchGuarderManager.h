47dd47848188856aeb612c5812336a0a56d552ee
chengsc <419923422@qq.com>
2021-04-23 10:39:33 +0800

    code struct

---
+++ LaunchGuarder/LaunchGuarder/LaunchGuarderManager.h
@@ -0,0 +1,16 @@
+//
+//  LaunchGuarderManager.h
+//  LaunchGuarder
+//
+//  Created by chengsc on 2021/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LaunchGuarderManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LaunchGuarder/LaunchGuarder/LaunchGuarderManager.m
@@ -0,0 +1,12 @@
+//
+//  LaunchGuarderManager.m
+//  LaunchGuarder
+//
+//  Created by chengsc on 2021/4/23.
+//
+
+#import "LaunchGuarderManager.h"
+
+@implementation LaunchGuarderManager
+
+@end
